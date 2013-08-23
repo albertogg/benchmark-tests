b243336a1c99cf5adf90b727dd33c4d0d17f11c3
Jonathan Trujillo <trujillo.jonathan@gmail.com>
2013-08-22 23:48:56 -0430

    dynamic port

--- express/app.js
+++ express/app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
 
-app.listen(3000);
-console.log('Listening to port 3000');
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log('Listening to port', port);
