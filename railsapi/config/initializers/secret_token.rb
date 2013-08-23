ea332d9564774128c64ae42089a3bfd56ce9cc0c
albertogg <albertogrespan@gmail.com>
2013-08-22 23:47:23 -0430

    Rails base.

---
+++ railsapi/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railsapi::Application.config.secret_key_base = '9d313ca62ddaa8b174fe1c06795622d174c7784e0527e934a7fb68b96b1d9ba4ed6f98ee960e31c9169ffed1842cd132f2606a85b4adc0454b8f375321511a51'
