1199982877c7e78b3753d883114018141f400c68
Jonathan Wiesel <jonathanwiesel@gmail.com>
2013-10-27 18:36:34 -0430

    agregado seeding de usuarios

--- express/app.js
+++ express/app.js
@@ -3,7 +3,8 @@ dotenv.load();
 
 var express = require('express'),
 	http    = require('http'),
- 	db  = require('./ormConfig');
+ 	db  = require('./ormConfig'),
+ 	seeds = require('./seeds');
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
@@ -14,6 +15,13 @@ db.sequelize.sync({ force: false }).complete(function(err) {
   	} else {
   		http.createServer(app).listen(app.get('port'), function() {
   			console.log('Express server listening on port ' + app.get('port') + '. env: ' + process.env.NODE_ENV);
+  			db.User.findAll().success(function(users){
+  				if(users.length == 0){
+  					db.User.bulkCreate(seeds.users).success(function(){
+  						console.log('seeds planted');
+  					})
+  				}
+  			});
   		});
   	}
 })
---
+++ express/seeds.js
@@ -0,0 +1,74 @@
+if (!global.hasOwnProperty('seeds')) {
+
+  var users = [
+    {
+      "lastName": "Cronin",
+      "email": "Arlene_Schultz@novella.name",
+      "firstName": "Thalia",
+      "country": "Martinique"
+    },
+    {
+      "lastName": "Parker",
+      "email": "Antonina.Crona@alycia.us",
+      "firstName": "Omari",
+      "country": "Seychelles"
+    },
+    {
+      "lastName": "Wolf",
+      "email": "Adolf@lance.me",
+      "firstName": "Kade",
+      "country": "Saint Martin"
+    },
+    {
+      "lastName": "Jacobs",
+      "email": "Sarina@dawson.tv",
+      "firstName": "Loren",
+      "country": "Chad"
+    },
+    {
+      "lastName": "Terry",
+      "email": "Alan_Monahan@maritza.name",
+      "firstName": "Tressa",
+      "country": "Slovenia"
+    },
+    {
+      "lastName": "Kris",
+      "email": "Zena_Sawayn@daron.us",
+      "firstName": "Nasir",
+      "country": "Brunei"
+    },
+    {
+      "lastName": "Cartwright",
+      "email": "Brain_Wiza@norbert.us",
+      "firstName": "Kendra",
+      "country": "Wake Island"
+    },
+    {
+      "lastName": "Vandervort",
+      "email": "Jamil.Auer@markus.biz",
+      "firstName": "Lawrence",
+      "country": "Azerbaijan"
+    },
+    {
+      "lastName": "Cruickshank",
+      "email": "Olin@lindsey.us",
+      "firstName": "Kolby",
+      "country": "Uruguay"
+    },
+    {
+      "lastName": "Walker",
+      "email": "Tara@adrianna.ca",
+      "firstName": "Zachariah",
+      "country": "Gambia"
+    }
+  ]
+
+
+  global.seeds = {
+
+    users:   users
+  }
+}
+
+module.exports = global.seeds
+
